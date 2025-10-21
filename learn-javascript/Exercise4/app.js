1e29c941f2b414ab89776c786bcf06fd6e7c1c07
joseph <dacleezy05@gmail.com>
2025-10-21 11:50:57 -0500

    add exercise4 folder and js file answers

---
+++ learn-javascript/Exercise4/app.js
@@ -0,0 +1,45 @@
+const emblemClue1 = "Eagle";
+const emblemClue2 = "Laurel";
+const emblemClue3 = 7;
+
+let locationStart = "Grapes";
+
+//if (emblemClue1 === "Eagle"); {
+//    locationStart = ("Forum");
+//
+//   elseif (emblemClue1 === "Lion");{
+//    locationStart = ("Colosseum");
+//}
+//    else{
+//        locationStart = ("Villa");
+//    }
+//}
+
+//if (emblemClue2 === "Laurel" && locationStart === "Forum")
+//    {
+//    locationStart += "of Augustus";
+//    }
+//    else if(emblemClue2 === "Grapes" || locationStart === "Villa")
+//    {
+//        locationStart += "of Pompey";
+//    }
+
+//switch (emblemClue3){
+//    case 7:
+//        locationStart += "North";
+//        break;
+//    
+//    case 3:
+//        locationStart+= "South";
+//        break;
+//    
+//    case 9:
+//        locationStart += "East";
+//        break;
+//    
+//    case 4:
+//        locationStart += "West";
+//        break;
+//}
+
+// Using === over == insures a direct match to a value with stricter equality checks.
