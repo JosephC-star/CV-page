27e05b2bc99ee3b74affd4cb28392da211a848bc
joseph <dacleezy05@gmail.com>
2025-10-22 15:40:33 -0500

    add exercise 5 code and answers

---
+++ learn-javascript/Exercise5/app.js
@@ -0,0 +1,32 @@
+const guests = ["ANTONY", "CICERO", "CASSIUS", "CLEOPATRA"];
+
+// Step 1
+
+//guests.unshift('BRUTUS');
+//guests[0]
+
+//Step 2
+
+//guests.push('AUGUSTUS', 'LUCIA');
+
+//Step 3
+
+//const sparticusIndex = guests.indexOf("SPARTICUS");
+//Sparticus.indexOf[-1]
+
+//Step 4
+
+//const indexToRemove = guests.indexOf("CASSIUS");
+//guests.splice(indexToRemove, 1);
+
+//Step 5
+
+//const specialGuests = guests.slice(0, 3);
+
+//Step 6
+
+// const honoredGuests = guests.slice(0, 1); // Extracts honored guests.
+// const otherGuests = guests.slice(1); // Extracts the rest of the guests.
+// otherGuests.sort(); // Sorts the other guests.
+// const sortedGuests = honoredGuests.concat(otherGuests); // Combines both arrays. 
+
