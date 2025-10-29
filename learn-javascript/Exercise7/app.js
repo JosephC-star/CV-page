4b9a9c3b02bacbb2fb27ab6ea4793ec08a95e649
joseph <dacleezy05@gmail.com>
2025-10-29 13:32:13 -0500

    add Exercise 7 answers

---
+++ learn-javascript/Exercise7/app.js
@@ -0,0 +1,17 @@
+const friend = "BRUTUS";
+const shiftValue = 3;
+
+const alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+// let encryptedName ="";
+
+// for (let i=0; i < friend.length; i++){
+//     const currentLetter = friend[i];
+//     const currentIndex = alphabet.indexOf(currentLetter.toLowerCase());
+//     const newIndex = (currentIndex + shiftValue) % alphabet.length;
+//     encryptedName += alphabet[newIndex]. toUpperCase();
+// }
+
+// Loops make it so programmers dont have to write redundant code. it automates the process without the extra code.
+
+// the % in the code ensures when we get to z it will wrap around to start at a again.
