1fd540102b67427b144cf818c01605463be43146
joseph <dacleezy05@gmail.com>
2025-11-04 16:01:02 -0600

    add fizzbuzz and letter count exercise

--- learn-javascript/Functions/panagram.js
+++ learn-javascript/Functions/panagram.js
@@ -1,29 +1,7 @@
-// If the number divides evenly by 3 and 5 print "Fizzbuzz
-// Else if divides evenly by 3 print "Fizz"
-// Else if divides evenly by 5 print "Buzz"
-// Else print the number.
-
-function fizzBuzz(n) {
-  if (!Number.isInteger(n) || n < 1 || n >= 100) {
-    console.log("Error");
-    return;
-  }
-  for (let i = 1; i <= n; i++) {
-    if (i % 3 === 0 && i % 5 === 0) {
-      console.log("FizzBuzz");
-    } else if (i % 3 === 0) {
-      console.log("Fizz");
-    } else if (i % 5 === 0) {
-      console.log("Buzz");
-    } else {
-      console.log(i);
-    }
-  }
-}
-
-fizzBuzz("k");
-fizzBuzz(100);
-fizzBuzz(16);
+// function letterOccurence(word){
+// if(typeof word !== "String");
+// console.log("Error");
+// return;
 
 function letterCount(word) {
   const alphabet = "abcdefghijklmnopqrstuvwxyz";
