19203d037b70b36b8e6e58066d497a562ae1ab03
joseph <dacleezy05@gmail.com>
2025-11-04 18:15:40 -0600

    add exercise8 folder and answers

---
+++ learn-javascript/Exercise8/app.js
@@ -0,0 +1,35 @@
+const friend = "BRUTUS";
+const shiftValue = 3;
+const alphabet = "abcdefghijklmnopqrstuvwxyz";
+
+// function enycryptLetter (letter, shift){
+// const index = alphabet.indexOf(letter.toLowerCase());
+// const newShiftValue = (index + shift) % alphabet.length;
+// return alphabet[newIndex];
+// }
+
+// function encryptMessage(word, shift) {
+//   let encryptMessage = "";
+//   for (let i = 0; i < word.length; i++) {
+//     encryptMessage += encryptedLetter(word[i], shift);
+//   }
+//   return encryptMessage;
+// }
+
+// function decryptLetter(enycryptLetter, shift) {
+//   const index = alphabet.indexOf(letter.toLowerCase());
+//   const newIndex = (index - shift + alphabet.length) % alphabet.length;
+//   return alphabet[newIndex];
+// }
+
+// function decryptMessage(word, shift) {
+//   let decryptMessage = "";
+//   for (i = 0; i < word.length; i++);
+//   {
+//     decryptMessage += decryptLetter(word[i], shift);
+//   }
+//   return decryptMessage;
+// }
+
+// If Caesar encrypts the word "BRUTUS" using our encryptMessage function and then decrypts the result using our decryptMessage function, will he get "BRUTUS" back? Why or why not?
+// Yes because the encrypt and decrypt will scramble and unscramble the message as intended.
